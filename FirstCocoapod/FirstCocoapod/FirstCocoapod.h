1dd46c9ba1e458c297f7c673e391a58511f3abce
Eimantas Kudarauskas <e.kudarauskas@gmail.com>
2018-08-21 17:33:00 +0300

    Finished first commit

---
+++ FirstCocoapod/FirstCocoapod/FirstCocoapod.h
@@ -0,0 +1,19 @@
+//
+//  FirstCocoapod.h
+//  FirstCocoapod
+//
+//  Created by Eimantas Kudarauskas on 8/21/18.
+//  Copyright © 2018 Eimantas Kudarauskas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FirstCocoapod.
+FOUNDATION_EXPORT double FirstCocoapodVersionNumber;
+
+//! Project version string for FirstCocoapod.
+FOUNDATION_EXPORT const unsigned char FirstCocoapodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirstCocoapod/PublicHeader.h>
+
+
---
+++ FirstCocoapod/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstCocoapod
+//
+//  Created by Eimantas Kudarauskas on 8/21/18.
+//  Copyright © 2018 Eimantas Kudarauskas. All rights reserved.
+//
+
+import Foundation
+
+
+public class ViewController {
+    
+    private init() { }
+    
+    public static func doSomething() -> String {
+        return "I'm doing something!!!"
+    }
+}
